43477b77f691abfb3fad7e2531e4618ed9ac939c
Ri4Chaard <yarik271202@gmail.com>
2024-06-28 00:42:28 +0300

    Removing an unused component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
 import { PasswordInputComponent } from './common-ui/password-input/password-input.component';
 
 @Component({
@@ -8,7 +7,7 @@ import { PasswordInputComponent } from './common-ui/password-input/password-inpu
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, ProfileCardComponent, PasswordInputComponent],
+  imports: [RouterOutlet, PasswordInputComponent],
 })
 export class AppComponent {
   title = 'my-app';
